c6120e65e44e72936c38af825d1749ce6db850d9
Emmanuel Nwafor <emmanuelnwafor39@gmail.com>
2023-04-13 10:43:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Nav from './components/navbar/Nav'
+import Carousel from './components/carousel/Carousel'
+import Form from './components/form/Form'
+import Footer from './components/footer/Footer'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <Carousel />
+      <Form />
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
